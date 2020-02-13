c664d730c1426c47fde2ff19f805b639dac74727
kononenkoAnton <a.kononenko@applicaster.com>
2020-02-13 17:45:28 -0500

    Feat: Add support of PushProviderProtocol (#14)

--- ZappPushPluginsSDK/BaseInstance/ZPPushProvider.swift
+++ ZappPushPluginsSDK/BaseInstance/ZPPushProvider.swift
@@ -9,17 +9,46 @@
 import UIKit
 import ZappPlugins
 
-@objc open class ZPPushProvider : NSObject, ZPPushProviderProtocol {
-    open var providerProperties: [String:NSObject]!
-    open var baseProperties: [String:NSObject] = [String:NSObject]()
-
-    open var configurationJSON:NSDictionary?
-    open var providerKey:String {
-        get {
-            return self.getKey()
-        }
+@objc open class ZPPushProvider: NSObject, ZPPushProviderProtocol, PushProviderProtocol {
+    public var model: ZPPluginModel?
+    public required init(pluginModel: ZPPluginModel) {
+        model = pluginModel
+        configurationJSON = model?.configurationJSON
     }
-    public required init(configurationJSON:NSDictionary?) {
+
+    public var providerName: String {
+        return getKey()
+    }
+
+    public func prepareProvider(_ defaultParams: [String: Any],
+                         completion: ((_ isReady: Bool) -> Void)?) {
+        guard configureProvider() == true else {
+            completion?(false)
+            return
+        }
+        defaultParams.forEach { dictionary in
+            guard let value = dictionary.value as? NSObject else {
+                return
+            }
+            setBaseParameter(value, forKey: dictionary.key)
+        }
+        completion?(true)
+    }
+
+    public func disable(completion: ((Bool) -> Void)?) {
+        // TODO: Must be implemented on superclass side
+        completion?(true)
+    }
+
+    open var providerProperties: [String: NSObject]!
+    open var baseProperties: [String: NSObject] = [String: NSObject]()
+
+    open var configurationJSON: NSDictionary?
+    open var providerKey: String {
+        return getKey()
+    }
+
+    public required init(configurationJSON: NSDictionary?) {
         super.init()
         self.configurationJSON = configurationJSON
     }
@@ -29,19 +58,18 @@ import ZappPlugins
     }
 
     open func getKey() -> String {
-        //implement in child classes
+        // implement in child classes
         return ""
     }
 
-    open func setBaseParameter(_ value:NSObject?, forKey key:String) {
+    open func setBaseParameter(_ value: NSObject?, forKey key: String) {
         if let value = value {
-            self.baseProperties[key] = value
+            baseProperties[key] = value
         }
     }
 
     open func configureProvider() -> Bool {
-        //implement in child classes
+        // implement in child classes
         return false
     }
-
 }
